bd98a7bbadb068620c24b07c3c0896131a2bab18
George Kartavenko <SocialImp@mail.ru>
2025-11-13 21:04:05 +0300

    :feat: some fix element Card

---
+++ src/data/card.ts
@@ -0,0 +1,34 @@
+import { CardItem } from '@/types/card';
+
+export const specialOffers: CardItem[] = [
+  {
+    id: 'statue-liberty',
+    image: '/img/travel/usa-best-places-new-york 1.png',
+    alt: 'Statue of Liberty',
+    title: 'Statue of Liberty',
+    location: 'New York',
+    rating: 4.8,
+    duration: '4 Day 5 Nights',
+    price: '$450/person',
+  },
+  {
+    id: 'great-wall',
+    image: '/img/travel/china-great-wall-and-mountain 1.png',
+    alt: 'The Great Wall',
+    title: 'The Great Wall',
+    location: 'China',
+    rating: 4.9,
+    duration: '7 Day 8 Nights',
+    price: '$850/person',
+  },
+  {
+    id: 'sajek-valley',
+    image: '/img/travel/Discover-sajek-valley 1.png',
+    alt: 'Sajek Valley',
+    title: 'Sajek Valley',
+    location: 'Bangladesh',
+    rating: 4.7,
+    duration: '3 Day 4 Nights',
+    price: '$400/person',
+  },
+];
---
+++ src/types/card.ts
@@ -0,0 +1,10 @@
+export interface CardItem {
+  id: string;
+  image: string;
+  alt: string;
+  title: string;
+  location: string;
+  rating: number;
+  duration: string;
+  price: string;
+}
